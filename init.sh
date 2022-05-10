bf16b0a8474f11c34f3c02cd174d92f6704ec026
EricHouang <erichouang@163.com>
2022-05-10 23:19:43 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Activate the plugin in ~/.zshrc"
 echo "plugins=( [plugins...] zsh-syntax-highlighting)"
-source ~.zshrc
+source ~/.zshrc
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/bin/.fzf
+~/bin/.fzf/install
+[ -f ~/bin/.fzf.zsh ] && source ~/bin/.fzf.zsh
 source ~/.zshrc
 echo ">>> fzf install sucessfully"
